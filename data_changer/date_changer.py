ac7d9511122331b571b5654f0cf9eaa92c0f1c0d
Missiks0 <misharus28062006@gmail.com>
2024-12-23 15:13:20 +1000

    Add files via upload

---
+++ data_changer/date_changer.py
@@ -0,0 +1,12 @@
+created_date = "19-03-2024"
+issue_date = "20-03-2024"
+
+temp_created_date = created_date[:5]
+temp_issue_date = issue_date[:5]
+
+print(f"Дата создания: {temp_created_date}")
+print(f"Дата выпуска: {temp_issue_date}")
+
+print(f"\nИсходные даты:")
+print(f"Дата создания: {created_date}")
+print(f"Дата выпуска: {issue_date}")
