f33bad3308104d7a576b6763c4cc9db036d93e70
Missiks0 <misharus28062006@gmail.com>
2024-12-25 19:11:17 +1000

    Add files via upload

---
+++  add_list/add_list.py
@@ -0,0 +1,9 @@
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+
+titles = [title1, title2, title3]
+
+print("Заголовки заметки:")
+for title in titles:
+    print(title)
